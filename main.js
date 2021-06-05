7b7dae583c317187d1b69b9da6a691766fdd4355
Priscilla-Rojas <priscilla.k.rojas@hotmail.com>
2021-06-05 17:43:42 -0300

    le agregue la transision al menu desplegable

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-const headerContainerMenu = document.getElementById('header-container-menu');
-const btnMenu = document.getElementById('container-hamburguesa');
-    
-btnMenu.addEventListener('click', (event)=>{
-    headerContainerMenu.classList.toggle('active');
-});
-
-var i = 0;
-var text = 'Front-End developer. Proactiva, responsable, entusiasta, curiosa y enfocada.';
+let i = 0;
+let text = 'Front-End developer. Proactiva, responsable, entusiasta, curiosa y enfocada.';
 type();
 function type(){
     if (i < text.length){
